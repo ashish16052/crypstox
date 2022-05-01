e3c599ee7e520f9f5b45cf108770fd6ca303fbf8
ashish <ashishmohite16052@gmail.com>
2022-05-01 18:11:17 +0530

    add chart js

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,18 +1,70 @@
 import React, { useEffect, useState } from 'react';
-import { SingleCoin } from '../components/api';
 import axios from 'axios';
+import { HistoricalChart } from '../components/api';
+import { Line } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
-const Graph = (props) => {
+const Graph = ({ coin }) => {
 
-    console.log(props.coin);
+    const [chartData, setChart] = useState();
+    const [days, setTime] = useState();
 
-    if (props.coin.id) {
+    const fetchData = async () => {
+        var id = coin.id ? coin.id : "bitcoin"
+        const { data } = await axios.get(HistoricalChart(id, days));
+        setChart(data.prices);
+    }
+    useEffect(() => {
+        fetchData();
+    }, [coin.id,days])
+
+    console.log(coin);
+
+    if (coin.id) {
         return (
             <div className='Graph'>
-                <img src={props.coin.image.small}></img><br></br>
-                {props.coin.id}<br></br>
-                {props.coin.market_data.current_price.inr}<br></br>
-                {props.coin.market_data.market_cap.inr}
+                <div className='CoinInfo'>
+                    <div className='Coinname'>
+                        <img src={coin.image.small}></img>
+                        <div>{coin.id}</div>
+                    </div>
+                    <div className='FilterButton'>
+                        <div className='ChartButton' onClick={()=>{setTime(1)}}>24 Hours</div>
+                        <div className='ChartButton' onClick={()=>{setTime(7)}}>7 Days</div>
+                        <div className='ChartButton' onClick={()=>{setTime(30)}}>30 Days</div>
+                    </div>
+                </div>
+                <Line
+                    data={{
+                        labels: chartData.map((coin) => {
+                            let date = new Date(coin[0]);
+                            let time =
+                                date.getHours() > 12
+                                    ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                                    : `${date.getHours()}:${date.getMinutes()} AM`;
+                            return days === 1 ? time : date.toLocaleDateString();
+                        }),
+
+                        datasets: [
+                            {
+                                data: chartData.map((coin) => coin[1]),
+                                label: `Price ( Past ${days} Days )`,
+                                borderColor: "#4F2ED0",
+                                backgroundColor: "#4F2ED0",
+                                color: "#4F2ED0"
+                            },
+                        ],
+                        
+                    }}
+                    options={{
+                        elements: {
+                            point: {
+                                radius: 1,
+                            },
+                        },
+                    }}
+                />
             </div>
         )
     }
--- src/components/api.js
+++ src/components/api.js
@@ -2,10 +2,10 @@ export const CoinList = () =>
   'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
 export const SingleCoin = (id) =>
-  'https://api.coingecko.com/api/v3/coins/'+id;
+  'https://api.coingecko.com/api/v3/coins/' + id;
 
 export const HistoricalChart = (id, days = 365, currency) =>
-  'https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}';
+  'https://api.coingecko.com/api/v3/coins/' + id + '/market_chart?vs_currency=inr&days=' + days;
 
 export const TrendingCoins = () =>
   'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=gecko_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h';
